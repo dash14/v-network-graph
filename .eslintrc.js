728d118cbb90e087439d701b3b10b866d98d9429
dash14 <dash14.ack@gmail.com>
2021-06-30 10:34:10 +0900

    Add ESLint, Prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/base",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: [
+    "vue",
+    "@typescript-eslint",
+    "prettier"
+  ],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? 2 : 0,
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/multiline-html-element-content-newline": 0,
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 3,
+        "allowFirstLine": true
+      }
+    }],
+    // "no-unused-vars": ["warn", { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { args: "after-used", argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+  },
+}
