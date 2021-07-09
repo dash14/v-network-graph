e2731e87a8e5fede26b92ef956b176aee5b37187
dash14 <dash14.ack@gmail.com>
2021-07-09 10:24:59 +0900

    Refactor simple/grid layout

--- src/components/layouts/simple.ts
+++ src/components/layouts/simple.ts
@@ -1,6 +1,6 @@
-import { toRef } from "@vue/reactivity"
+import { Ref, toRef } from "@vue/reactivity"
 import { watch } from "@vue/runtime-core"
-import { NodePositions, OnDragHandler } from "@/common/types"
+import { NodePositions, OnDragHandler, Position } from "@/common/types"
 import { getNodeSize, areNodesCollision } from "@/common/utility"
 import { LayoutActivateParameters, LayoutHandler } from "./handler"
 
@@ -10,26 +10,24 @@ export class SimpleLayout implements LayoutHandler {
   private onDeactivate?: () => void
 
   activate(parameters: LayoutActivateParameters): void {
-    const { layouts, nodes, styles, emitter, svgPanZoom, zoomLevel } = parameters
+    const { layouts, nodes, styles, emitter, scale, svgPanZoom } = parameters
     const onDrag: OnDragHandler = positions => {
       for (const [id, pos] of Object.entries(positions)) {
         const layout = this.getOrCreateNodePosition(layouts, id)
-        layout.value.x = pos.x
-        layout.value.y = pos.y
+        this.setNodePosition(layout, pos)
       }
     }
 
     const stopNodeWatch = watch(
       () => Object.keys(nodes),
       (nodeIds) => {
-        if (!svgPanZoom.value) return
         // decide new node's position
         const newNodes = nodeIds.filter(n => !(n in layouts))
-        const area = svgPanZoom.value.getViewArea()
-        const scale = styles.view.resizeWithZooming ? 1 : zoomLevel.value
+        const area = svgPanZoom.getViewArea()
+        const s = scale.value
         for (const nodeId of newNodes) {
           const node = nodes[nodeId]
-          const nodeSize = getNodeSize(node, styles.node, scale)
+          const nodeSize = getNodeSize(node, styles.node, s)
           const candidate = { ...area.center }
           for (;;) {
             let collision = false
@@ -37,25 +35,26 @@ export class SimpleLayout implements LayoutHandler {
               if (nodeId === id) continue
               const targetNode = nodes[id]
               if (!targetNode) continue
-              const targetNodeSize = getNodeSize(targetNode, styles.node, scale)
+              const targetNodeSize = getNodeSize(targetNode, styles.node, s)
               collision = areNodesCollision(candidate, nodeSize, pos, targetNodeSize)
               if (collision) {
                 break
               }
             }
-            if (collision) { // shift to new position
-              candidate.x += nodeSize.width + NEW_NODE_POSITION_MARGIN / scale
+            if (collision) {
+              // Slide the width of one node + margin in the horizontal direction.
+              // If it reaches the edge of the display area, it moves downward.
+              candidate.x += nodeSize.width + NEW_NODE_POSITION_MARGIN / s
               if (candidate.x + nodeSize.width / 2 > area.box.right) {
                 candidate.x = area.center.x
-                candidate.y += nodeSize.height + NEW_NODE_POSITION_MARGIN / scale
+                candidate.y += nodeSize.height + NEW_NODE_POSITION_MARGIN / s
               }
             } else {
               break
             }
           }
           const layout = this.getOrCreateNodePosition(layouts, nodeId)
-          layout.value.x = candidate.x
-          layout.value.y = candidate.y
+          this.setNodePosition(layout, candidate)
         }
       }
     )
@@ -75,6 +74,11 @@ export class SimpleLayout implements LayoutHandler {
     if (this.onDeactivate) {
       this.onDeactivate()
     }
+  }
+
+  protected setNodePosition(nodeLayout: Ref<Position>, pos: Position) {
+    nodeLayout.value.x = pos.x
+    nodeLayout.value.y = pos.y
   }
 
   private getOrCreateNodePosition(layouts: NodePositions, node: string) {
