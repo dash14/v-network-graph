1f724ca0ab29377db7db11802bf317ce72770972
dash14 <dash14.ack@gmail.com>
2021-07-25 18:46:33 +0900

    Remove dead codes

--- src/Example.vue
+++ src/Example.vue
@@ -328,12 +328,11 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive, ref, watch } from "vue"
+import { defineComponent, reactive, ref, watch } from "vue"
 import throttle from "lodash-es/throttle"
 import VNetworkGraph from "./components/network-graph.vue"
 import { cloneAllConfigDefaults } from "./common/config-defaults"
 import { UserLayouts, Nodes, Edges, LayerPos } from "./common/types"
-import { SimpleLayout } from "./layouts/simple"
 import { GridLayout } from "./layouts/grid"
 import { ForceLayout } from "./layouts/force"
 import { LayoutHandler } from "./layouts/handler"
