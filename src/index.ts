219237773d52671261cba97eb2bc80b99f2101e6
dash14 <dash14.ack@gmail.com>
2021-08-01 14:32:34 +0900

    Export getFullConfigs function

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export default install
 // each can be registered via Vue.component()
 export * from "./components/index"
 
-export { getConfigDefaults } from "./common/config-defaults"
+export { getFullConfigs } from "./common/config-defaults"
 
 export { SimpleLayout } from "./layouts/simple"
 export { GridLayout } from "./layouts/grid"
