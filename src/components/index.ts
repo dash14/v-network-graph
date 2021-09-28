5f47a5068c0f76f12d47c92fd4ee409d08311543
dash14 <dash14.ack@gmail.com>
2021-09-28 10:21:39 +0900

    Export VLabelText component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@
 export { default as VNetworkGraph } from "./network-graph.vue"
 
 export { default as VEdgeLabel } from "./edge-label.vue"
+
+export { default as VLabelText } from "./label-text.vue"
