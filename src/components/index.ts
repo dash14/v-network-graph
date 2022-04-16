3fe8a6e9b5152f01b8a8130fc488b5273eecc4de
dash14 <dash14.ack@gmail.com>
2022-04-17 00:46:54 +0900

    Expose VShape components for use cases that override nodes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 
 export { default as VNetworkGraph } from "./VNetworkGraph.vue"
 
+export { default as VShape } from "./base/VShape.vue"
+
 export { default as VEdgeLabel } from "./edge/VEdgeLabel.vue"
 
 export { default as VLabelText } from "./base/VLabelText.vue"
