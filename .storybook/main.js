afc34b2132993b073bb29010a4beb11ff1d65368
dash14 <dash14.ack@gmail.com>
2022-10-23 20:08:53 +0900

    Setup storybook 7

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+}
